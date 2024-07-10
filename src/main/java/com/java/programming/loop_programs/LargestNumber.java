12af5de7084d6323f275ac8776f42d43d2aa3b02
Rajat0808 <rajatwankhede124@gmail.com>
2024-07-10 23:39:29 +0530

    Program4

---
+++ src/main/java/com/java/programming/loop_programs/LargestNumber.java
@@ -0,0 +1,30 @@
+package com.java.programming.loop_programs;
+
+import java.util.Scanner;
+
+/*
+Write a Java program to find the largest digit  in the given series of number.
+
+Description :
+Input : 12321
+Output : 3
+ */
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in)  ;
+        int num  = s.nextInt() ;
+        int largest_digit = num %10  ;
+
+        num /= 10  ;
+        while( num != 0 ){
+            int digit = num % 10  ;
+            if( digit > largest_digit)
+            {
+                largest_digit = digit ;
+            }
+            num  /= 10 ;
+        }
+        System.out.println(largest_digit);
+    }
+    }
+}
